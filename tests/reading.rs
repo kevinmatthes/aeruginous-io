281a20d94c03108f12aa4c69741995366fff8c26
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2024-01-10 21:06:44 +0100

    [Enhancement] Configure Rust Code (#4)
    * Added ::= Cargo.lock
    
    * Skip ::= fix copyright years
    
    * Added ::= .gitignore
    
    * Added ::= .github/workflows/mirror.yml
    
    * Added ::= Cargo.toml
    
    * Added ::= src/lib.rs
    
    * Added ::= .clippy.toml
    
    * Skip ::= add CI steps
    
    * Added ::= .github/workflows/pr.yml
    
    * Added ::= .github/release.yml
    
    * Added ::= .github/CODEOWNERS
    
    * Added ::= ReadPathBufLike
    
    * Added ::= src/reading/read_path_buf_like.rs
    
    * Added ::= tests/assets/GPL-3.0.rs
    
    * Added ::= src/reading/mod.rs
    
    * Added ::= tests/reading.rs
    
    * Added ::= dependency:  Rust crate ``sysexits``
    
    * Skip ::= update Cargo.lock
    
    * Skip ::= cffconvert .github/aeruginous-io.cff
    
    * Added ::= .github/aeruginous-io.cff
    
    * Skip ::= fix typo
    
    * Skip ::= fix typo
    
    * Skip ::= fix typo
    
    * Added ::= .github/workflows/cff.yml
    
    * Added ::= ReadPathBufLike::read_silently
    
    * Added ::= ReadPathBufLike::read_loudly
    
    * Skip ::= add missing documentation
    
    * Create summary of recent changes
    
    ---------
    
    Co-authored-by: GitHub Actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ tests/reading.rs
@@ -0,0 +1,62 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2024 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+use aeruginous_io::ReadPathBufLike;
+
+#[test]
+fn read_path_buf_like_failure_directory() {
+    assert!(".github/".read_loudly().is_err());
+    assert!(".github/".read_silently().is_err());
+}
+
+#[test]
+fn read_path_buf_like_failure_does_not_exist() {
+    assert!("no_such_file.txt".read_loudly().is_err());
+    assert!("no_such_file.txt".read_silently().is_err());
+}
+
+#[test]
+fn read_path_buf_like_success() {
+    assert_eq!(
+        "tests/assets/GPL-3.0.rs".read_loudly().unwrap(),
+        "tests/assets/GPL-3.0.rs".read_silently().unwrap(),
+    );
+
+    assert_eq!(
+        "tests/assets/GPL-3.0.rs".read_silently().unwrap(),
+        "\
+/// Copyright (C) 2024 Kevin Matthes
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"
+    );
+}
+
+/******************************************************************************/
