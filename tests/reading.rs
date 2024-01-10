76df02bd9d12bd135716b4d912cc786ef8a13a05
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2024-01-10 15:27:39 +0100

    Added ::= tests/reading.rs

---
+++ tests/reading.rs
@@ -0,0 +1,55 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2024 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+use aeruginous_io::ReadPathBufLike;
+
+#[test]
+fn read_path_buf_like_failure_directory() {
+    assert!(".github/".read().is_err());
+}
+
+#[test]
+fn read_path_buf_like_failure_does_not_exist() {
+    assert!("no_such_file.txt".read().is_err());
+}
+
+#[test]
+fn read_path_buf_like_success() {
+    assert_eq!(
+        "tests/assets/GPL-3.0.rs".read().unwrap(),
+        "\
+/// Copyright (C) 2023 Kevin Matthes
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"
+    );
+}
+
+/******************************************************************************/
