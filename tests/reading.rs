383ccf9035f5f10b5ef328de0005f9109b1d0b93
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2024-01-14 21:20:59 +0100

    Added ::= OptionReader::read_loudly

--- tests/reading.rs
+++ tests/reading.rs
@@ -17,54 +17,78 @@
 |                                                                              |
 \******************************************************************************/
 
-use aeruginous_io::{BufReadReader, PathBufLikeReader};
+mod buf_read_reader {
+    use aeruginous_io::BufReadReader;
 
-#[test]
-fn read_buf_reader_failure() {
-    assert!("👍"
-        .as_bytes()
-        .iter()
-        .map(|b| if b > &200 { 0 } else { *b })
-        .collect::<Vec<u8>>()
-        .read_loudly()
-        .is_err());
-    assert!("👍"
-        .as_bytes()
-        .iter()
-        .map(|b| if b > &200 { 0 } else { *b })
-        .collect::<Vec<u8>>()
-        .read_silently()
-        .is_err());
+    #[test]
+    fn read_loudly_failure() {
+        assert!("👍"
+            .as_bytes()
+            .iter()
+            .map(|b| if b > &200 { 0 } else { *b })
+            .collect::<Vec<u8>>()
+            .read_loudly()
+            .is_err());
+    }
+
+    #[test]
+    fn read_loudly_success() {
+        assert_eq!({ &(b"test"[..]) }.read_loudly().unwrap(), "test\n");
+    }
+
+    #[test]
+    fn read_silently_failure() {
+        assert!("👍"
+            .as_bytes()
+            .iter()
+            .map(|b| if b > &200 { 0 } else { *b })
+            .collect::<Vec<u8>>()
+            .read_silently()
+            .is_err());
+    }
+
+    #[test]
+    fn read_silently_success() {
+        assert_eq!({ &(b"test"[..]) }.read_silently().unwrap(), "test\n");
+    }
 }
 
-#[test]
-fn read_buf_read_success() {
-    assert_eq!({ &(b"test"[..]) }.read_loudly().unwrap(), "test\n");
-    assert_eq!({ &(b"test"[..]) }.read_silently().unwrap(), "test\n");
-}
+mod path_buf_like_reader {
+    use aeruginous_io::PathBufLikeReader;
 
-#[test]
-fn read_path_buf_like_failure_directory() {
-    assert!(".github/".read_loudly().is_err());
-    assert!(".github/".read_silently().is_err());
-}
+    #[test]
+    fn method_result_equality() {
+        assert_eq!(
+            "tests/assets/GPL-3.0.rs".read_loudly().unwrap(),
+            "tests/assets/GPL-3.0.rs".read_silently().unwrap(),
+        );
+    }
 
-#[test]
-fn read_path_buf_like_failure_does_not_exist() {
-    assert!("no_such_file.txt".read_loudly().is_err());
-    assert!("no_such_file.txt".read_silently().is_err());
-}
+    #[test]
+    fn read_loudly_failure_attempt_to_read_directory() {
+        assert!(".github/".read_loudly().is_err());
+    }
 
-#[test]
-fn read_path_buf_like_success() {
-    assert_eq!(
-        "tests/assets/GPL-3.0.rs".read_loudly().unwrap(),
-        "tests/assets/GPL-3.0.rs".read_silently().unwrap(),
-    );
+    #[test]
+    fn read_loudly_failure_file_does_not_exist() {
+        assert!("no_such_file.txt".read_loudly().is_err());
+    }
 
-    assert_eq!(
-        "tests/assets/GPL-3.0.rs".read_silently().unwrap(),
-        "\
+    #[test]
+    fn read_silently_failure_attempt_to_read_directory() {
+        assert!(".github/".read_silently().is_err());
+    }
+
+    #[test]
+    fn read_silently_failure_file_does_not_exist() {
+        assert!("no_such_file.txt".read_silently().is_err());
+    }
+
+    #[test]
+    fn read_silently_success() {
+        assert_eq!(
+            "tests/assets/GPL-3.0.rs".read_silently().unwrap(),
+            "\
 /// Copyright (C) 2024 Kevin Matthes
 ///
 /// This program is free software: you can redistribute it and/or modify
@@ -80,7 +104,62 @@ fn read_path_buf_like_success() {
 /// You should have received a copy of the GNU General Public License
 /// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 "
-    );
+        );
+    }
+}
+
+mod option_reader {
+    use aeruginous_io::OptionReader;
+
+    #[test]
+    fn method_result_equality_none() {
+        assert_eq!(
+            None::<&str>.read_loudly(&b"test"[..]).unwrap(),
+            None::<&str>.read_silently(&b"test"[..]).unwrap()
+        );
+    }
+
+    #[test]
+    fn method_result_equality_some() {
+        assert_eq!(
+            Some("tests/assets/GPL-3.0.rs")
+                .read_loudly(&b""[..])
+                .unwrap(),
+            Some("tests/assets/GPL-3.0.rs")
+                .read_silently(&b""[..])
+                .unwrap(),
+        );
+    }
+
+    #[test]
+    fn read_silently_success_none() {
+        assert_eq!(None::<&str>.read_silently(&b"test"[..]).unwrap(), "test\n");
+    }
+
+    #[test]
+    fn read_silently_success_some() {
+        assert_eq!(
+            Some("tests/assets/GPL-3.0.rs")
+                .read_silently(&b""[..])
+                .unwrap(),
+            "\
+/// Copyright (C) 2024 Kevin Matthes
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"
+        );
+    }
 }
 
 /******************************************************************************/
