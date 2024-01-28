e5e9b6c19fee4e14e7936c07a8a888bd8259ad0a
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2024-01-28 20:02:34 +0100

    Added ::= OptionTruncation

--- src/writing.rs
+++ src/writing.rs
@@ -120,10 +120,84 @@ where
     }
 }
 
+/// Truncate either destination, depending on the circumstances.
+pub trait OptionTruncation<P, W>
+where
+    PathBuf: From<P>,
+    W: Write,
+{
+    /// Truncate either destination, depending on the circumstances.
+    ///
+    /// This method behaves just like
+    /// [`crate::OptionTruncation::truncate_silently`] despite also printing
+    /// error messages to [`std::io::Stderr`].
+    ///
+    /// # Errors
+    ///
+    /// See [`sysexits::ExitCode`].
+    fn truncate_loudly(
+        self,
+        destination: Option<P>,
+        alternative: W,
+    ) -> Result<()>;
+
+    /// Truncate either destination, depending on the circumstances.
+    ///
+    /// The data this method is called on will be converted to a [`String`] and
+    /// written to either source.  The data therefore needs to implement
+    /// [`ToString`].  If the default destination is [`Some`], it will be
+    /// truncated by [`crate::PathBufLikeTruncation::truncate_silently`].  The
+    /// default destination therefore needs to implement
+    /// [`crate::PathBufLikeTruncation`].  If the default
+    /// destination is [`None`], the alternative will be used as output stream.
+    /// The alternative therefore needs to implement [`crate::Writer`].
+    ///
+    /// The return value is either the unit type, in case of success, or a
+    /// [`sysexits::ExitCode`] to describe the error cause, otherwise.
+    ///
+    /// Error messages are not written to [`std::io::Stderr`].
+    ///
+    /// # Errors
+    ///
+    /// See [`sysexits::ExitCode`].
+    fn truncate_silently(
+        self,
+        destination: Option<P>,
+        alternative: W,
+    ) -> Result<()>;
+}
+
+impl<P, W: Write, T: ToString> OptionTruncation<P, W> for T
+where
+    PathBuf: From<P>,
+{
+    fn truncate_loudly(
+        self,
+        destination: Option<P>,
+        alternative: W,
+    ) -> Result<()> {
+        match destination {
+            Some(p) => PathBufLikeTruncation::truncate_loudly(self, p),
+            None => self.write_loudly(alternative),
+        }
+    }
+
+    fn truncate_silently(
+        self,
+        destination: Option<P>,
+        alternative: W,
+    ) -> Result<()> {
+        match destination {
+            Some(p) => PathBufLikeTruncation::truncate_silently(self, p),
+            None => self.write_silently(alternative),
+        }
+    }
+}
+
 /// Write to a [`std::io::Write`]r.
 pub trait Writer<T>
 where
-    T: std::io::Write,
+    T: Write,
 {
     /// Write the data this method is called on to the given destination.
     ///
