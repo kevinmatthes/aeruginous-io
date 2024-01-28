cd9b0a61531b6023574802d99f4f1134a597fee6
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2024-01-28 20:03:07 +0100

    Added::= OptionTruncation::truncate_silently

--- src/lib.rs
+++ src/lib.rs
@@ -132,7 +132,7 @@ mod reading;
 mod writing;
 
 pub use reading::{BufReadReader, OptionReader, PathBufLikeReader};
-pub use writing::{PathBufLikeTruncation, Writer};
+pub use writing::{OptionTruncation, PathBufLikeTruncation, Writer};
 
 /// This crate's name.
 pub const NAME: &str = "aeruginous-io";
