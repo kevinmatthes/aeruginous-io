9b794a35f115776f3cf33fa6a3ab89700c5e9768
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2024-02-11 16:09:27 +0100

    [Enhancement] Append Data to ``PathBuf``-like Instances (#111)
    * Added ::= PathBufLikeAppendix
    
    * Added ::= PathBufLikeAppendix::append_silently
    
    * Added ::= PathBufLikaAppendix::append_loudly
    
    * Changed ::= MSRV:  1.76.0
    
    * Added ::= tests for PathBufLikeAppendix
    
    * Create summary of recent changes
    
    * Skip ::= fix typo
    
    ---------
    
    Co-authored-by: GitHub Actions <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/reading.rs
+++ tests/reading.rs
@@ -53,6 +53,60 @@ mod buf_read_reader {
     }
 }
 
+mod option_reader {
+    use aeruginous_io::OptionReader;
+
+    #[test]
+    fn method_result_equality_none() {
+        assert_eq!(
+            None::<&str>.read_loudly(&b"test"[..]).unwrap(),
+            None::<&str>.read_silently(&b"test"[..]).unwrap()
+        );
+    }
+
+    #[test]
+    fn method_result_equality_some() {
+        assert_eq!(
+            Some("tests/assets/GPL-3.0.rs")
+                .read_loudly(&b""[..])
+                .unwrap(),
+            Some("tests/assets/GPL-3.0.rs")
+                .read_silently(&b""[..])
+                .unwrap(),
+        );
+    }
+
+    #[test]
+    fn read_silently_success_none() {
+        assert_eq!(None::<&str>.read_silently(&b"test"[..]).unwrap(), "test\n");
+    }
+
+    #[test]
+    fn read_silently_success_some() {
+        assert_eq!(
+            Some("tests/assets/GPL-3.0.rs")
+                .read_silently(&b""[..])
+                .unwrap(),
+            "\
+/// Copyright (C) 2024 Kevin Matthes
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"
+        );
+    }
+}
+
 mod path_buf_like_reader {
     use aeruginous_io::PathBufLikeReader;
 
@@ -88,60 +142,6 @@ mod path_buf_like_reader {
     fn read_silently_success() {
         assert_eq!(
             "tests/assets/GPL-3.0.rs".read_silently().unwrap(),
-            "\
-/// Copyright (C) 2024 Kevin Matthes
-///
-/// This program is free software: you can redistribute it and/or modify
-/// it under the terms of the GNU General Public License as published by
-/// the Free Software Foundation, either version 3 of the License, or
-/// (at your option) any later version.
-///
-/// This program is distributed in the hope that it will be useful,
-/// but WITHOUT ANY WARRANTY; without even the implied warranty of
-/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-/// GNU General Public License for more details.
-///
-/// You should have received a copy of the GNU General Public License
-/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"
-        );
-    }
-}
-
-mod option_reader {
-    use aeruginous_io::OptionReader;
-
-    #[test]
-    fn method_result_equality_none() {
-        assert_eq!(
-            None::<&str>.read_loudly(&b"test"[..]).unwrap(),
-            None::<&str>.read_silently(&b"test"[..]).unwrap()
-        );
-    }
-
-    #[test]
-    fn method_result_equality_some() {
-        assert_eq!(
-            Some("tests/assets/GPL-3.0.rs")
-                .read_loudly(&b""[..])
-                .unwrap(),
-            Some("tests/assets/GPL-3.0.rs")
-                .read_silently(&b""[..])
-                .unwrap(),
-        );
-    }
-
-    #[test]
-    fn read_silently_success_none() {
-        assert_eq!(None::<&str>.read_silently(&b"test"[..]).unwrap(), "test\n");
-    }
-
-    #[test]
-    fn read_silently_success_some() {
-        assert_eq!(
-            Some("tests/assets/GPL-3.0.rs")
-                .read_silently(&b""[..])
-                .unwrap(),
             "\
 /// Copyright (C) 2024 Kevin Matthes
 ///
